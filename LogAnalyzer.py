6d0e33d50a3e0503b8b526287e4c3e8ec87cebe4
KevinPro <kevin79713@gmail.com>
2021-10-19 15:13:49 +0800

    情境4: 同時使用 虛設常式與模擬物件 兩個假物件

--- LogAnalyzer.py
+++ LogAnalyzer.py
@@ -4,6 +4,10 @@ import abc
 # 情境1: 先做一個手刻假物件
 # 情境2: 改用隔離框架
 # 情境3: 用隔離框架, 模擬有回傳值的方法
+# 情境4:
+    # 同時使用 虛設常式與模擬物件 兩個假物件
+    # log 紀錄拋出例外
+    # Anylazer 接收例外後, 呼叫 webService 方法
 '''
 
 
@@ -19,11 +23,18 @@ class IFileNameRules(metaclass=abc.ABCMeta):
         pass
 
 
+class IWebService(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def write(errMsg: str) -> None:
+        pass
+
+
 class LogAnalyzer:
-    def __init__(self, logger: ILogger, rulesUtil: IFileNameRules = None) -> None:
+    def __init__(self, logger: ILogger, rulesUtil: IFileNameRules = None, webService: IWebService = None) -> None:
         self.minNameLength = 5
         self.__logger__ = logger
         self.__rulesUtil__ = rulesUtil
+        self.__webService__ = webService
         pass
 
     def validSomeRules(self, fileName: str):
@@ -31,6 +42,9 @@ class LogAnalyzer:
 
     def analyze(self, fileName: str):
         if(len(fileName) < self.minNameLength):
-            self.__logger__.logError('too short')
+            try:
+                self.__logger__.logError('too short')
+            except Exception as e:
+                self.__webService__.write('mocked log error')
 
         pass
--- test_LogAnalyzer.py
+++ test_LogAnalyzer.py
@@ -30,8 +30,28 @@ def test_analyze_tooShortFileName_callLogger(mocker):
     # 以前去驗證方法有沒有被呼叫, 所以可以去掉(mock 框架已提供了驗證方法). 為了方便說明所以用 mark
     # assert 'too short' == logger.lastError
 
+
 def test_validSomeRules_notValidName_ReturnFalse(mocker):
     fakeObject = mocker.patch('LogAnalyzer.IFileNameRules')
-    mocker.patch('LogAnalyzer.IFileNameRules.isValidLogFileName', return_value=False) # 方法設定回傳值
+    mocker.patch('LogAnalyzer.IFileNameRules.isValidLogFileName', return_value=False)  # 方法設定回傳值
     analyzer = LogAnalyzer(None, fakeObject)
     assert False == analyzer.validSomeRules('abc.txt')
+
+
+def test_analyze_logThrow_callWebService(mocker):
+    # 虛設常式
+    logger = mocker.patch('LogAnalyzer.ILogger')  
+    mocker.patch.object(logger, 'logError', side_effect=Exception('mocked log error'))  # throw
+
+    # 模擬物件
+    webService = mocker.patch('LogAnalyzer.IWebService')
+    mocker_method = mocker.patch.object(webService, 'write')
+
+    analyzer = LogAnalyzer(logger, webService=webService)
+    analyzer.minNameLength = 6
+    analyzer.analyze('a.txt')
+    assert mocker_method.called == True
+    assert mocker_method.call_args.args[0] == 'mocked log error'  # check params[0]
+
+    # if arguments is multi, can use syntax of below:
+    # assert mocker_method.call_args.args == ('metadata_example', 'action_example')
